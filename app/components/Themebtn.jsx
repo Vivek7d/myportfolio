204da2ef9650d2d883433b936a2c18d8fa867f0a
Vivek7d <vivekdhalkari07@gmail.com>
2024-03-09 15:31:34 +0530

    link and logos fixed

--- app/components/Themebtn.jsx
+++ app/components/Themebtn.jsx
@@ -3,31 +3,37 @@
 import { FiSun, FiMoon } from "react-icons/fi";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
+import Image from "next/image";
 
 export default function Themebtn() {
   const [mounted, setMounted] = useState(false);
-  const { setTheme, resolvedTheme, setDefaultTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-    setDefaultTheme('light'); // Set default theme to light
-  }, []);
+  useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
+  if (!mounted)
+    return (
+      <Image
+        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+        width={40}
+        height={40}
+        sizes="36x36"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      />
+    );
 
-  return (
-    <div className="flex items-center">
-      {resolvedTheme === "light" ? (
-        <FiMoon
-          className="h-7 w-7 cursor-pointer"
-          onClick={() => setTheme("dark")}
-        />
-      ) : (
-        <FiSun
-          className="h-7 w-7 cursor-pointer"
-          onClick={() => setTheme("light")}
-        />
-      )}
-    </div>
-  );
+  if (resolvedTheme === "dark") {
+    return (
+      <FiSun
+        className="h-7 w-7 cursor-pointer"
+        onClick={() => setTheme("light")}
+      />
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return <FiMoon className="h-7 w-7 cursor-pointer" onClick={() => setTheme("dark")} />;
+  }
 }
